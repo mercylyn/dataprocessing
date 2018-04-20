302800d9bd4c5887d23fb1c1b8abf7b37bd2cebc
mercylyn <m.d.wiemer@gmail.com>
2018-04-20 20:09:17 +0200

    style verbeteren

--- homework/week_2/temp1981.js
+++ homework/week_2/temp1981.js
@@ -1,9 +1,13 @@
-/*
+/**
  * Name: Mercylyn Wiemer
  * Course: Data Processing
  * Date: 20-04-2018
  *
- * Maximum temperature in De Bilt (NL) 1981
+ * Maximum temperature in De Bilt (NL) 1981.
+ * This program creates a line graph of the temperature at De Bilt weather
+ * station. The graph is included in a web page on github.
+ *
+ * Source of data: http://projects.knmi.nl/klimatologie/daggegevens/selectie.cgi
  *
  */
 
@@ -11,90 +15,97 @@ function reqListener ()
 {
     dom = this.responseText;
 
-    var knmi = dom.split("\n", 365);
-    var dateArray = [];
-    var tempArray = [];
+    let knmi_split = dom.split("\n", 365);
+    let dateArray = [];
+    let tempArray = [];
 
-    for(var i = 0; i < knmi.length; i++)
+    // Store dates and temperatures in individual arrays.
+    for(let i = 0; i < knmi_split.length; i++)
     {
-        var data = knmi[i].split(",");
-        var dateIsoFormat = data[0].replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
+        let data = knmi_split[i].split(",");
+        let dateIsoFormat = data[0].replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
         dateArray.push(new Date(dateIsoFormat));
         tempArray.push(+data[1]);
     }
 
-    var canvas = document.getElementById("canvas");
-    //if (canvas.getContext)
-    var ctx = canvas.getContext('2d');
-
-    var dateTransform = createTransform([dateArray[0], dateArray[364]], [50, 595]);
-    var tempTransform = createTransform([Math.max(...tempArray), Math.min(...tempArray)], [50, 325]);
-
-
-    var transformedDate = []
-    var transformedTemp = []
-
-    for(var i = 0; i < dateArray.length; i++)
+    let canvas = document.getElementById("canvas");
+    if (canvas.getContext)
     {
-        transformedDate.push(dateTransform(dateArray[i]));
-        transformedTemp.push(tempTransform(tempArray[i]));
+        let ctx = canvas.getContext('2d');
+
+        /* create functions to transform dates and temperatures. */
+        let dateTransform = createTransform([dateArray[0], dateArray[364]], [50, 595]);
+        let tempTransform = createTransform([Math.max(...tempArray), Math.min(...tempArray)], [50, 325]);
+
+
+        let transformedDate = []
+        let transformedTemp = []
+
+        /* transform the data: dates and temperatures. */
+        for(let i = 0; i < dateArray.length; i++)
+        {
+            transformedDate.push(dateTransform(dateArray[i]));
+            transformedTemp.push(tempTransform(tempArray[i]));
+        }
+
+        /* Draw graph: temperature per day. */
+        ctx.beginPath();
+        for(let i = 0; i < transformedDate.length; i++)
+        {
+            ctx.moveTo(transformedDate[i], transformedTemp[i]);
+            ctx.lineTo(transformedDate[i + 1], transformedTemp[i + 1]);
+        }
+        ctx.stroke();
+
+        // let transMaxTemp = Math.min(...transformedTemp);
+        // var transMinTemp = Math.max(...transformedTemp);
+        // var MaxTemp = Math.max(...tempArray);
+        // var MinTemp = Math.min(...tempArray);
+
+        /* Draw the x-axis. */
+        ctx.beginPath();
+        ctx.moveTo(transformedDate[0], tempTransform(-150));
+        ctx.lineTo(transformedDate[364], tempTransform(-150));
+
+        /* Draw the y-axis. */
+        ctx.moveTo(40, tempTransform(250));
+        ctx.lineTo(40, tempTransform(-150));
+
+        let yAxis = [-150, -100, -50, 0, 50, 100, 150, 200, 250];
+        let xAxis = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30];
+
+        /* Draw the values of y-axis (temperature). */
+        for (let i = 0; i < yAxis.length; i++)
+        {
+            // values of y-axis
+            ctx.moveTo(40, tempTransform(yAxis[i]));
+            ctx.lineTo(30, tempTransform(yAxis[i]));
+
+            ctx.font = "12px Arial";
+            ctx.fillText(String(yAxis[i]), 5, tempTransform(yAxis[i]) + 5);
+
+        }
+
+
+        let month_days = 0;
+        let month_name = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        /* Draw the values of the x-axis (month name). */
+        for (let i = 0; i < xAxis.length; i++)
+        {
+            ctx.moveTo(transformedDate[month_days += xAxis[i]], tempTransform(-150));
+            ctx.lineTo(transformedDate[month_days], tempTransform(-160));
+
+            ctx.font = "12px Arial";
+            ctx.fillText(String(month_name[i]), transformedDate[month_days] + 10, tempTransform(-170));
+        }
+
+        ctx.stroke();
+
+        /* Title. */
+        ctx.font = "20px Arial";
+        ctx.fillText("Maximum Temperature in De Bilt (NL) 1981", 140, 30);
     }
-
-    console.log(transformedDate[31]);
-
-    ctx.beginPath();
-    for(var i = 0; i < dateArray.length; i++)
-    {
-        ctx.moveTo(transformedDate[i], transformedTemp[i]);
-        ctx.lineTo(transformedDate[i + 1], transformedTemp[i + 1]);
-    }
-    ctx.stroke();
-
-    // var transMaxTemp = Math.min(...transformedTemp);
-    // var transMinTemp = Math.max(...transformedTemp);
-    // var MaxTemp = Math.max(...tempArray);
-    // var MinTemp = Math.min(...tempArray);
-
-    // x-axis
-    ctx.beginPath();
-    ctx.moveTo(transformedDate[0], tempTransform(-150));
-    ctx.lineTo(transformedDate[364], tempTransform(-150));
-
-    // y-axis
-    ctx.moveTo(40, tempTransform(250));
-    ctx.lineTo(40, tempTransform(-150));
-
-    var yAxis = [-150, -100, -50, 0, 50, 100, 150, 200, 250];
-    var xAxis = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 30];
-
-    for (var i = 0; i < yAxis.length; i++)
-    {
-        // values of y-axis
-        ctx.moveTo(40, tempTransform(yAxis[i]));
-        ctx.lineTo(30, tempTransform(yAxis[i]));
-
-        ctx.font = "12px Arial";
-        ctx.fillText(String(yAxis[i]), 5, tempTransform(yAxis[i]) + 5);
-
-    }
-
-
-    var month = 0;
-    var month_name = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    for (var i = 0; i < xAxis.length; i++)
-    {
-        ctx.moveTo(transformedDate[month += xAxis[i]], tempTransform(-150));
-        ctx.lineTo(transformedDate[month], tempTransform(-160));
-
-        ctx.font = "12px Arial";
-        ctx.fillText(String(month_name[i]), transformedDate[month] + 10, tempTransform(-170));
-    }
-
-    ctx.stroke();
-
-    ctx.font = "20px Arial";
-    ctx.fillText("Maximum Temperature in De Bilt (NL) 1981", 140, 30);
 
     function createTransform(domain, range)
     {
@@ -105,24 +116,24 @@ function reqListener ()
     	// range_max = alpha * domain_max + beta
      		// a solution would be:
 
-        var domain_min = domain[0]
-        var domain_max = domain[1]
-        var range_min = range[0]
-        var range_max = range[1]
+        let domain_min = domain[0]
+        let domain_max = domain[1]
+        let range_min = range[0]
+        let range_max = range[1]
 
         // formulas to calculate the alpha and the beta
-       	var alpha = (range_max - range_min) / (domain_max - domain_min)
-        var beta = range_max - alpha * domain_max
+       	let alpha = (range_max - range_min) / (domain_max - domain_min)
+        let beta = range_max - alpha * domain_max
 
         // returns the function for the linear transformation (y= a * x + b)
         return function(x)
         {
-          return alpha * x + beta;
+            return alpha * x + beta;
         }
     }
 }
 
-var oReq = new XMLHttpRequest();
+let oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
 oReq.open("GET", "https://raw.githubusercontent.com/mercylyn/dataprocessing/master/homework/week_2/data_1981.csv");
 oReq.send();
